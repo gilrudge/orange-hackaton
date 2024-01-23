79c163675023352dfca4a516f7c8834fb9e8876b
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-23 10:37:40 -0300

    Criando a entidade User

--- backend/orange/src/main/java/com/hackathon/orange/model/User.java
+++ backend/orange/src/main/java/com/hackathon/orange/model/User.java
@@ -1,6 +1,30 @@
 
 package com.hackathon.orange.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long  id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String sobrenome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
 }
